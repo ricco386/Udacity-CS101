1d4382cffb8fbd5c4c6a6d9e51d25f3a2b05b1ac
Richard Kellner <richard.kellner@ajty.info>
2012-08-30 01:32:27 +0200

    Unit 2 Quiz 29

--- crawler.py
+++ crawler.py
@@ -1,12 +1,22 @@
-# Write Python code that assigns to the 
-# variable url a string that is the value 
-# of the first URL that appears in a link 
-# tag in the string page.
+# Modify the get_next_target procedure so that
+# if there is a link it behaves as before, but
+# if there is no link tag in the input string,
+# it returns None, 0.
 
-# page = contents of a web page
-page =('<div id="top_bin"><div id="top_content" class="width960">'
-		'<div class="udacity float-left"><a href="http://www.xkcd.com">')
-start_link = page.find('<a href=')
-start_url = start_link + page[start_link:].find('"') +1
-end_url = start_url + page[start_url+1:].find('"') +1
-url = page[start_url:end_url]
+# Note that None is not a string and so should
+# not be enclosed in quotes.
+
+# Also note that your answer will appear in
+# parentheses if you print it.
+
+def get_next_target(page):
+	start_link = page.find('<a href=')
+
+	#Insert your code below here
+	if start_link == -1:
+		return None, 0
+		    
+	start_quote = page.find('"', start_link)
+	end_quote = page.find('"', start_quote + 1)
+	url = page[start_quote + 1:end_quote]
+	return url, end_quote
