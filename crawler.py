8afd7558d13d01ff631f5800bdfc423aaea493c6
Richard Kellner <richard.kellner@ajty.info>
2012-08-30 01:18:31 +0200

    Unit 1 Final Quiz

--- crawler.py
+++ crawler.py
@@ -1,8 +1,12 @@
-# Write Python code that initializes the variable
-# start_link to be the value of the position
-# where the first '<a href=' occurs in a page.
+# Write Python code that assigns to the 
+# variable url a string that is the value 
+# of the first URL that appears in a link 
+# tag in the string page.
 
-page = '''<div id="top_bin"> <div id="top_content" class="width960">
-   <div class="udacity float-left"> <a href="/">'''
-
+# page = contents of a web page
+page =('<div id="top_bin"><div id="top_content" class="width960">'
+		'<div class="udacity float-left"><a href="http://www.xkcd.com">')
 start_link = page.find('<a href=')
+start_url = start_link + page[start_link:].find('"') +1
+end_url = start_url + page[start_url+1:].find('"') +1
+url = page[start_url:end_url]
