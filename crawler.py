7556e285ebbdf64743e2e92e35206c7c832a8c3f
Richard Kellner <richard.kellner@ajty.info>
2012-09-02 19:15:55 +0200

    Unit 4 Problem set 4 Question 5

--- crawler.py
+++ crawler.py
@@ -52,7 +52,8 @@ def crawl_web(seed):
 def add_to_index(index,keyword,url):
     for entry in index:
         if entry[0] == keyword:
-            entry[1].append(url)
+            if not url in entry[1]:
+                entry[1].append(url)
             return
     index.append([keyword, [url]])
 
