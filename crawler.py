a3d7445415e88ef117f045586440b87f66344d62
Richard Kellner <richard.kellner@ajty.info>
2012-09-02 20:17:41 +0200

    Added comments to the crwawler functions

--- crawler.py
+++ crawler.py
@@ -1,5 +1,10 @@
-#Finish crawl web
+#WEB Crawler
 
+#
+# Functions required for crawling the web
+#
+
+# Function that get requested url from the Internet
 def get_page(url):
     try:
         import urllib
@@ -7,6 +12,7 @@ def get_page(url):
     except:
         return ""
 
+# Help function that return first link and end position of the link
 def get_next_target(page):
     start_link = page.find('<a href=')
     if start_link == -1:
@@ -16,12 +22,13 @@ def get_next_target(page):
     url = page[start_quote + 1:end_quote]
     return url, end_quote
 
+# Help function union two arrays
 def union(p,q):
     for e in q:
         if e not in p:
             p.append(e)
 
-
+# Function that extracts all links from requested page
 def get_all_links(page):
     links = []
     while True:
@@ -33,6 +40,7 @@ def get_all_links(page):
             break
     return links
 
+# Initial function that starts the web crawling on a particular URL
 def crawl_web(seed):
     tocrawl = [seed]
     crawled = []
@@ -46,9 +54,11 @@ def crawl_web(seed):
             crawled.append(page)
     return index
 
+#
+# Functions required for indexing, crawled pages
+#
 
-
-# Indexing
+# Function that add word to the index
 def add_to_index(index,keyword,url):
     for entry in index:
         if entry[0] == keyword:
@@ -59,17 +69,20 @@ def add_to_index(index,keyword,url):
             return
     index.append([keyword, [[url, 0]]])
 
+# Help function that search the index for the keyword
 def lookup(index,keyword):
     for entry in index:
         if entry[0] == keyword:
         return entry[1]
     return []
 
+# Function that splits page into words and adds them to index
 def add_page_to_index(index,url,content):
     words = content.split()
     for keyword in words:
         add_to_index(index,keyword,url)
 
+# Function for recording cliks on a particular link
 def record_user_click(index,keyword,url):
     urls = lookup(index, keyword)
         if urls:
