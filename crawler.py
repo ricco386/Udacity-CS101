46b739277e70ea4213bd71cfbc7c10f4ac59bfe4
Richard Kellner <richard.kellner@ajty.info>
2012-09-08 19:07:22 +0200

    Unit 6 Quiz 28

--- crawler.py
+++ crawler.py
@@ -90,9 +90,44 @@ def record_user_click(index,keyword,url):
             if entry[0] == url:
                 entry[1] += 1
 
+# Function that computes page ranks
+#
+# Formula to count rank:
+#
+# rank(page, 0) = 1/npages
+# rank(page, t) = (1-d)/npages
+#                 + sum (d * rank(p, t - 1) / number of outlinks from p)
+#                 over all pages p that link to this page
+#
+def compute_ranks(graph):
+    d = 0.8 #dumping constant
+    numloops = 40
+
+    ranks = {}
+    npages = len(graph)
+    for page in graph:
+        ranks[page] = 1.0 / npages
+
+    for i in range(0, numloops):
+        newranks = {}
+        for page in graph:
+            newrank = (1 - d) / npages
+            #Loop throught all pages
+            for node in graph:
+                #check if node links to page
+                if page in graph[node]:
+                    #Add to new rank based on this node
+                    newrank += d * ranks[node] / len(graph[node])
+            newranks[page] = newrank
+        ranks = newranks
+    return ranks
+
 index, graph = crawl_web('http://www.udacity.com/cs101x/urank/index.html');
 
-print "\n Index: \n"
-print index
-print "\n Graph: \n"
-print graph
+#print "\n Index: \n"
+#print index
+#print "\n Graph: \n"
+#print graph
+
+ranks = compute_ranks(graph)
+print ranks
